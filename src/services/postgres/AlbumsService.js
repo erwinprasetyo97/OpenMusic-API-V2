d5595a128ed0859a38e4952cff594b7614a181e5
erwin <erwinsetyoprasetyo@gmail.com>
2023-06-12 21:10:20 +0700

    Feedback after reviewed

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -1,37 +1,31 @@
-/* eslint-disable eol-last */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable indent */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable linebreak-style */
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
 const { mapDBToModelAlbums } = require('../../utils');
- 
+
 class AlbumsService {
   constructor() {
     this._pool = new Pool();
   }
 
   async addAlbum({ name, year }) {
-    const id = nanoid(16);
+    const id = `album-${nanoid(16)}`;
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
- 
+
     const query = {
       text: 'INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id',
       values: [id, name, year, createdAt, updatedAt],
     };
- 
+
     const result = await this._pool.query(query);
 
     if (!result.rows[0].id) {
-        throw new InvariantError('Album gagal ditambahkan');
-      }
-   
-      return result.rows[0].id;
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 
   async getAlbumById(id) {
@@ -40,11 +34,11 @@ class AlbumsService {
       values: [id],
     };
     const result = await this._pool.query(query);
- 
+
     if (!result.rows.length) {
       throw new NotFoundError('Album tidak ditemukan');
     }
- 
+
     return result.rows.map(mapDBToModelAlbums)[0];
   }
 
@@ -54,9 +48,9 @@ class AlbumsService {
       text: 'UPDATE albums SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id',
       values: [name, year, updatedAt, id],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (!result.rows.length) {
       throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan');
     }
@@ -67,9 +61,9 @@ class AlbumsService {
       text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
       values: [id],
     };
- 
+
     const result = await this._pool.query(query);
- 
+
     if (!result.rows.length) {
       throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
     }
